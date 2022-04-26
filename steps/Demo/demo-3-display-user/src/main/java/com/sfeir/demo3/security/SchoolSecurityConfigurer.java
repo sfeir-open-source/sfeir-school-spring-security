65524cb28d231f9259c7147cc6b499b0a17bebe2
Sébastien De Santis <desantis.s@sfeir.com>
2022-04-26 18:51:55 +0200

    Add an in memory user in demo 3

--- steps/Demo/demo-3-display-user/src/main/java/com/sfeir/demo3/security/SchoolSecurityConfigurer.java
+++ steps/Demo/demo-3-display-user/src/main/java/com/sfeir/demo3/security/SchoolSecurityConfigurer.java
@@ -1,8 +1,15 @@
 package com.sfeir.demo3.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 public class SchoolSecurityConfigurer extends WebSecurityConfigurerAdapter {
@@ -18,5 +25,19 @@ public class SchoolSecurityConfigurer extends WebSecurityConfigurerAdapter {
                 .antMatchers("/private").authenticated()
                 .antMatchers("/public").permitAll()
                 .anyRequest().authenticated();
+    }
+    @Bean
+    public UserDetailsService userDetailsManager(){
+      InMemoryUserDetailsManager userDetailsManager = new InMemoryUserDetailsManager();
+
+      UserDetails userDetails = User.withUsername("Luke").password("spacerunner").authorities("read").build();
+
+      userDetailsManager.createUser(userDetails);
+
+      return userDetailsManager;
+    }
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+      return NoOpPasswordEncoder.getInstance();
     }
 }
