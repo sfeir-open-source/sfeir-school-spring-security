f2b05c8ea2ab52c8cbb706ddcbbe6941d1082f86
Sébastien De Santis <desantis.s@sfeir.com>
2022-04-27 08:13:02 +0200

    add unit tests

--- steps/Solutions/solution-1/src/test/java/com/sfeir/solution1/Solution1ApplicationTests.java
+++ steps/Solutions/solution-1/src/test/java/com/sfeir/solution1/Solution1ApplicationTests.java
@@ -1,13 +1,38 @@
 package com.sfeir.solution1;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.httpBasic;
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class Solution1ApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+  @Autowired
+  private MockMvc mvc;
+
+  @Test
+  void getAnonymousdUser_whenNavigateToPublic_thenDisplayData() throws Exception {
+    var contentAsString = mvc.perform(get("/public"))
+      .andExpect(status().isOk())
+      .andReturn().getResponse().getContentAsString();
+
+    assertEquals("Everyone can see this ;)", contentAsString);
+  }
+  @Test
+  void getAuthenticatedUser_whenNavigateToPrivate_thenDisplayData() throws Exception {
+    var contentAsString = mvc.perform(get("/private").with(httpBasic("student","student")))
+      .andExpect(status().isOk())
+      .andReturn().getResponse().getContentAsString();
+
+    assertEquals("Welcome student! Your roles are VISITOR.", contentAsString);
+  }
 
 }
